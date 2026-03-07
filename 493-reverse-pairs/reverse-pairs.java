3e6f58b85d64070826314638f79e0a98636fa14d
YashGawandare <155295440+YashGawandare@users.noreply.github.com>
2026-03-07 20:32:02 +0530

    Time: 42 ms (84.62%) | Memory: 64.6 MB (39.07%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.java
+++ 493-reverse-pairs/reverse-pairs.java
@@ -1,35 +1,35 @@
 class Solution {
     static int count=0;
-    public static void conquer(int[] arr,int si,int mid,int ei){
+    public static void conquer(int[] arr, int si,int mid,int ei){
         int[] merged=new int[ei-si+1];
-        int indx1=si;
-        int indx2=mid+1;
+        int idx1=si;
+        int idx2=mid+1;
         int x= 0;
-        while(indx1<=mid && indx2<=ei){
-            if(arr[indx1]<=arr[indx2]){
-                merged[x++]=arr[indx1++];
+        while(idx1<=mid && idx2<=ei){
+            if(arr[idx1]<=arr[idx2]){
+                merged[x++]=arr[idx1++];
             }
             else{
-                merged[x++]=arr[indx2++];
-                
+                merged[x++]=arr[idx2++];
             }
         }
-        while(indx1<=mid)  merged[x++]=arr[indx1++];
-        while(indx2<=ei)  merged[x++]=arr[indx2++];
+        while(idx1<=mid) merged[x++] = arr[idx1++];
+        while(idx2<=ei) merged[x++] = arr[idx2++];
         for(int i=0,j=si;i<merged.length;j++,i++){
             arr[j]=merged[i];
         }
+
     }
-        public static void countPairs(int[] arr,int si,int mid,int ei){
-            int j=mid+1;
-            for(int i=si;i<=mid;i++){
-                while(j<=ei && arr[i]>2L*arr[j]){
-                    j++;
-                }
-                count+=(j-(mid+1));
+    public static void countPairs(int[] arr, int si ,int mid ,int ei){
+        int j=mid+1;
+        for(int i=si;i<=mid;i++){
+            while(j<=ei && arr[i]>2L * arr[j]){
+                j++;
             }
+            count+=(j-(mid+1));
         }
-    public static void divide(int[] arr, int si, int ei){
+    }
+    public static void divide(int[] arr,int si,int ei){
         if(si>=ei) return;
         int mid=si+(ei-si)/2;
         divide(arr,si,mid);
